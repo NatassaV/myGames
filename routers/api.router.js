67b90482d3e496db25b8a7b5c7eff5a2c521a7df
NatassaV <annbonni@gmail.com>
2021-12-03 13:46:56 +0000

    add change

--- routers/api.router.js
+++ routers/api.router.js
@@ -11,7 +11,9 @@ apiRouter.use("/reviews", reviewsRouter);
 apiRouter.use("/comments", commentsRouter);
 
 apiEndpoints = (req, res, next) => {
-  res.status(200).send({ endpoints: {"Here are some endpoints you can use" : endpoints });
+  res
+    .status(200)
+    .send({ endpoints: { "Here are some endpoints you can use": endpoints } });
 };
 
 apiRouter.use("", apiEndpoints);
