d0fe73bea81ed69a4c671a15f75eff7c3d40ce2f
Natassa <91126114+NatassaV@users.noreply.github.com>
2022-02-15 22:36:25 +0000

    Update api.router.js

--- routers/api.router.js
+++ routers/api.router.js
@@ -1,22 +1,13 @@
 const express = require("express");
-const endpoints = require("../endpoints.json");
+
 const apiRouter = express.Router();
 
 const categoriesRouter = require("./categories.router");
 const reviewsRouter = require("./reviews.router");
 const commentsRouter = require("./comments.router");
-const anyTable = require("../controllers/reviews.controller");
 
 apiRouter.use("/categories", categoriesRouter);
 apiRouter.use("/reviews", reviewsRouter);
 apiRouter.use("/comments", commentsRouter);
-
-apiRouter.use("*", reviewsRouter);
-
-apiEndpoints = (req, res, next) => {
-  res.status(200).send({ "Here are some endpoints you can use": endpoints });
-};
-
-apiRouter.use("", apiEndpoints);
 
 module.exports = apiRouter;
