c404327f7c06a7fbe8ad8e6d48b4b21d7d3b9b64
Natassa <91126114+NatassaV@users.noreply.github.com>
2022-02-15 22:21:53 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routers/api.router");
+const {
+  handlePsqlErrors400,
+  handleCustomErrors,
+  handle500Errors,
+  handlesInvalidPaths,
+} = require("./errors");
 
 app.use(express.json());
-
 app.use("/api", apiRouter);
-
-app.all("/*", (req, res) => {
-  res.status(404).send({ msg: "Route not found" });
-});
-
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send({ msg: "int server error" });
-});
+app.all("/*", handlesInvalidPaths);
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors400);
+app.use(handle500Errors);
 
 module.exports = app;
